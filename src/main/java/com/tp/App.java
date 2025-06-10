521bdbb1457ec730551ae04584816546fa643f5f
Valery M. <projets.monthe@gmail.com>
2025-06-10 03:37:13 +0100

    Initial commit

---
+++ src/main/java/com/tp/App.java
@@ -0,0 +1,17 @@
+package com.tp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int addition(int a, int b) 
+    { 
+        return a + b; 
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tp/AppTest.java
@@ -0,0 +1,17 @@
+package com.tp;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest 
+    
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testAddition() 
+    {
+        int result = App.addition(2, 3);
+        assertTrue(result == 5);
+    }
+}
